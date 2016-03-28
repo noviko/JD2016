6ec541515fc9a3a569e9d7a8b2e89c97e8eb9063
chatovich <chatovich@gmail.com>
2016-03-28 12:35:28 +0300

    JD02_09 B начало

--- src/by/it/chetovich/JD02_09/manual/Diagnosis.java
+++ src/by/it/chetovich/JD02_09/manual/Diagnosis.java
@@ -5,41 +5,109 @@ import javax.xml.bind.annotation.adapters.CollapsedStringAdapter;
 import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
 
 /**
- * Created by user_2 on 28.03.2016.
+ * Diagnosis
  */
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.FIELD)     // FIELD - все поля в XML
+@XmlType(name = "Diagnosis", propOrder = {  // PROPERTY - только  с геттерами и сеттерами
+        "name",                           // PUBLIC_MEMBER - только public
+        "drugs",                       // NONE - ни одно
+        "recoveryTime"
+})
+
 public class Diagnosis {
 
-    @XmlRootElement
-    @XmlAccessorType(XmlAccessType.FIELD)     // FIELD - все поля в XML
-    @XmlType(name = "Student", propOrder = {  // PROPERTY - только  с геттерами и сеттерами
-            "name",                           // PUBLIC_MEMBER - только public
-            "nickname",                       // NONE - ни одно
-            "telephone",
-            "address"
-    })
-    public class Student {
-        @XmlAttribute(required = true) //опишем логин как аттрибут
-        @XmlJavaTypeAdapter(CollapsedStringAdapter.class) //тип адаптера
-        @XmlID                         //и ключевой (уникальный)
-        private String login;
-        @XmlElement(required = true)
-        private String name;
-        @XmlElement(required = true)
-        private String nickname;
-        @XmlAttribute(required = false)
-        private String faculty;
-        @XmlElement(required = true)
-        private int telephone;
-        @XmlElement(required = true)
-        private Address address = new Address();
-        public Student() { } // необходим для маршаллизации/демаршалиизации XML
+    @XmlAttribute(required = true) //опишем логин как аттрибут
+    @XmlJavaTypeAdapter(CollapsedStringAdapter.class) //тип адаптера
+    @XmlID                         //и ключевой (уникальный)
+    private String id;
+    @XmlElement(required = true)
+    private String name;
+    @XmlElement(required = true)
+    private String recoveryTime;
+    @XmlElement(required = true)
+    private Drugs drugs = new Drugs();
 
-        public Student(String login, String name, String nickname, String faculty, int telephone, Address address) {
-            this.login = login;
-            this.name = name;
-            this.nickname = nickname;
-            this.faculty = faculty;
-            this.telephone = telephone;
-            this.address = address;
+    public Diagnosis() {
+    } // необходим для маршаллизации/демаршалиизации XML
+
+    public Diagnosis(String id, String name, String recoveryTime, Drugs drugs) {
+        this.id = id;
+        this.name = name;
+        this.recoveryTime = recoveryTime;
+        this.drugs = drugs;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getRecoveryTime() {
+        return recoveryTime;
+    }
+
+    public Drugs getDrugs() {
+        return drugs;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setRecoveryTime(String recoveryTime) {
+        this.recoveryTime = recoveryTime;
+    }
+
+    public void setAddress(Drugs drugs) {
+        this.drugs = drugs;
+    }
+
+    public String toString() {
+        return "\nID: " + id + "\nName: " + name + "\nRecovery Time: " + recoveryTime
+                + drugs.toString();
+    }
+
+
+    @XmlRootElement
+    @XmlType(name = " drugs ", propOrder = {
+            "name",
+            "dosage"
+    })
+    public static class Drugs {
+        private String name;
+        private String dosage;
+
+        //конструкторы
+        public Drugs() {// необходим для маршаллизации/демаршалиизации XML
         }
+
+        public Drugs(String name, String dosage) {
+            this.name = name;
+            this.dosage = dosage;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getDosage() {
+            return dosage;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public void setDosage(String dosage) {
+            this.dosage = dosage;
+        }
+    }
 }
