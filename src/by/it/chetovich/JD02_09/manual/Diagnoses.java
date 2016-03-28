62eda15ae924fd2d5aa65d839fdb8ea2ed9207c6
Yultos <chatovich@gmail.com>
2016-03-28 17:10:19 +0300

    JD02_09 B

--- src/by/it/chetovich/JD02_09/manual/Diagnoses.java
+++ src/by/it/chetovich/JD02_09/manual/Diagnoses.java
@@ -3,15 +3,18 @@ package by.it.chetovich.JD02_09.manual;
 import by.it.akhmelev.JD02_09.manual.Student;
 
 import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
 import java.util.ArrayList;
 
 /**
  * diagnoses
  */
+@XmlRootElement
+
 public class Diagnoses {
 
     @XmlElement(name="diagnosis")
-    private ArrayList<Diagnosis> list = new ArrayList<Diagnosis>();
+    private ArrayList<Diagnosis> list = new ArrayList<>();
     public Diagnoses() {
         super();
     }
--- src/by/it/chetovich/JD02_09/manual/Hospital.java
+++ src/by/it/chetovich/JD02_09/manual/Hospital.java
@@ -1,7 +1,43 @@
 package by.it.chetovich.JD02_09.manual;
 
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+
 /**
- * Created by user_2 on 28.03.2016.
+ * hospital
  */
+@XmlRootElement
+
 public class Hospital {
+
+    @XmlElement(name="diagnoses")
+    private Diagnoses diagnoses;
+    @XmlElement(name="prescribings")
+    private Prescribings prescribings;
+    public Hospital() {
+        super();
+    }
+
+    public void setDiagnoses(Diagnoses diagnoses) {
+        this.diagnoses = diagnoses;
+    }
+
+    public void setPrescribings(Prescribings prescribings) {
+        this.prescribings = prescribings;
+    }
+
+   /* public Diagnoses getDiagnoses() {
+        return diagnoses;
+    }
+
+    public Prescribings getPrescribings() {
+        return prescribings;
+    }*/
+
+
+    @Override
+    public String toString() {
+        return "Diagnoses [" + diagnoses + "]"+'\n'+"Prescribings [" + prescribings + "]";
+    }
 }
