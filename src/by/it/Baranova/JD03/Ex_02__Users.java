5294fa778cf93e835b51fecf33ad8015c7848fa8
katerinabaranova <katebaranova@me.com>
2016-04-04 19:04:22 +0300

    JD03-01 A-B

---
+++ src/by/it/Baranova/JD03/Ex_02__Users.java
@@ -0,0 +1,26 @@
+package by.it.Baranova.JD03;
+
+
+import java.sql.*;
+
+
+public class Ex_02__Users {
+
+
+    public static void main(String[ ] args) {
+        try (Connection connection=
+                     DriverManager.getConnection
+                             (CN.URL_DB,CN.USER_DB,CN.PASSWORD_DB);
+
+             Statement statement=connection.createStatement();) {
+            statement.executeUpdate(
+                    "insert into Users (Login,Password,Name,Surname,Phonenumber,Mail,FKRole)"+
+                    "values('poval','povale','Egor','Povalev',375295673425,'poval_e@mail.ru','2');"
+            );
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
+}
