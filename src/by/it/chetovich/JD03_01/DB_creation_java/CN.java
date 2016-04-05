c076e387a7ae2b9e63c756d55813e37feeeb76dd
Yultos <chatovich@gmail.com>
2016-04-05 12:57:14 +0300

    JD03_02 CRUD operations

--- src/by/it/chetovich/JD03_01/DB_creation_java/CN.java
+++ src/by/it/chetovich/JD03_01/DB_creation_java/CN.java
@@ -5,7 +5,7 @@ package by.it.chetovich.JD03_01.DB_creation_java;
  */
 public class CN {
 
-    public static final String URL_DB = "jdbc:mysql://127.0.0.1:2016/searchhost_java";
+    public static final String URL_DB = "jdbc:mysql://127.0.0.1:3306/searchhost_java";
     public static final String USER_DB = "root";
-    public static final String PASSWORD_DB = "";
+    public static final String PASSWORD_DB = "yultosik";
 }
---
+++ src/by/it/chetovich/JD03_02/crud/Temp.java
@@ -0,0 +1,13 @@
+package by.it.chetovich.JD03_02.crud;
+
+/**
+ * Temporary class for checking sql queries
+ */
+public class Temp {
+
+    public static void main(String[] args) {
+
+        CRUD_operations.updateUsers("2","Dasha","Ivanova",null,null, null, "2", null);
+        //CRUD_operations.insertCities("Kazan");
+    }
+}
