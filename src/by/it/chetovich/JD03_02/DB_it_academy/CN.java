2f9bd9242def23b3644af3efa698a75d12defc9d
Yultos <chatovich@gmail.com>
2016-04-05 13:15:55 +0300

    JD03_02 DB it-academy

---
+++ src/by/it/chetovich/JD03_02/DB_it_academy/CN.java
@@ -0,0 +1,11 @@
+package by.it.chetovich.JD03_02.DB_it_academy;
+
+/**
+ * CN
+ */
+public class CN {
+
+    public static final String URL_DB = "jdbc:mysql://127.0.0.1:3306/it-academy";
+    public static final String USER_DB = "root";
+    public static final String PASSWORD_DB = "yultosik";
+}
---
+++ src/by/it/chetovich/JD03_02/DB_it_academy/KillingDB.java
@@ -0,0 +1,33 @@
+package by.it.chetovich.JD03_02.DB_it_academy;
+
+import by.it.chetovich.JD03_01.DB_creation_java.*;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+/**
+ * Drop DB
+ */
+public class KillingDB {
+
+    public static void main(String[] args) {
+
+        try(Connection connection = DriverManager.getConnection(CN.URL_DB, CN.USER_DB, CN.PASSWORD_DB);
+            Statement statement = connection.createStatement()) {
+
+            statement.executeUpdate("drop table feedbacks;");
+            statement.executeUpdate("drop table profiles;");
+            statement.executeUpdate("drop table cities;");
+            statement.executeUpdate("drop table users;");
+            statement.executeUpdate("drop table role;");
+            //statement.executeUpdate("drop database it-academy;");
+
+            System.out.println("Tables from DB it-academy were deleted");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
