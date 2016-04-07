e2ad7bef2ec7741d2111657de3a4d349897bba4e
Dolf <dmitry.dailidovich@gmail.com>
2016-04-07 18:05:01 +0300

    JD03_01 sql requests update2.

--- src/by/it/daylidovich/JD03_01/SQL/create/table_Tours.sql
+++ src/by/it/daylidovich/JD03_01/SQL/create/table_Tours.sql
@@ -1,10 +1,10 @@
 create table tours(
 ID int AUTO_INCREMENT,
-FK_country varchar(20) NOT NULL,
-FK_type_tour varchar(15) NOT NULL,
-FK_transport varchar(15) NOT NULL,
-FK_type_hotel varchar(15) NOT NULL,
-FK_food_complex varchar(25) NOT NULL,
+FK_country int(3) NOT NULL,
+FK_type_tour int(1) NOT NULL,
+FK_transport int(1) NOT NULL,
+FK_type_hotel int(1) NOT NULL,
+FK_food_complex int(1) NOT NULL,
 Cost int NOT NULL,
 Discount int NOT NULL,
 primary key(ID)
