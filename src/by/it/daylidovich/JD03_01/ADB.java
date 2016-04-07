4ec6e2a1e6350e1eb9803bb85d870c789acd074e
Dolf <dmitry.dailidovich@gmail.com>
2016-04-07 12:57:08 +0300

    JD03_01 create ADB.

---
+++ src/by/it/daylidovich/JD03_01/ADB.java
@@ -0,0 +1,7 @@
+package by.it.daylidovich.JD03_01;
+
+public class ADB {
+    public static final String URL_DB = "jdbc:mysql://localhost:2016/travel_agency";
+    public static final String USER_DB = "root";
+    public static final String PASSWORD_DB = "";
+}
