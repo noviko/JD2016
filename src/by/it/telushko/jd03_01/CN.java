2f9ac0abde2cd1e210367988894aaa5c15108d73
Телушко <vladimir.telushko@gmail.com>
2016-04-09 02:22:48 +0300

    добавлено : jd03_01 C

---
+++ src/by/it/telushko/jd03_01/CN.java
@@ -0,0 +1,10 @@
+package by.it.telushko.jd03_01;
+
+/**
+ * Created by user_2 on 04.04.2016.
+ */
+public class CN {
+    public static final String URL_DB="jdbc:mysql://127.0.0.1:3306";
+    public static final String USER_DB="root";
+    public static final String PASSWORD_DB = "";
+}
