b8959c35c24d69680b0c8993c0eb58d4116c478e
Khmelov <Khmelov@users.noreply.github.com>
2016-04-13 10:36:54 +0300

    sync2

--- src/projects/akhmelev/servlets/hello.java
+++ src/projects/akhmelev/servlets/hello.java
@@ -1,17 +1,18 @@
- import java.io.PrintWriter;
-  
-  public class hello extends HttpServlet {
- -    @Override
- -    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
- -        PrintWriter out = resp.getWriter();
- -        out.println("Hello world");
- -        out.flush(); //ВАЖНО! Иначе вывод будет неустойчивым...
- +        @Override
- +        protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
- +            PrintWriter out = resp.getWriter();
- +            out.println("Hello world");
- +            out.flush(); //ВАЖНО! Иначе вывод будет неустойчивым...
-  
- -    }
- +        }
-  
+package projects.akhmelev.servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class hello extends HttpServlet {
+        @Override
+        protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+            PrintWriter out = resp.getWriter();
+            out.println("Hello world");
+            out.flush(); //ВАЖНО! Иначе вывод будет неустойчивым...
+
+        }
+}
