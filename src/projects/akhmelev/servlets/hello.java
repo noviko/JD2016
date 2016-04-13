88329bdf1ef6a9eb0e260a6187ba0f216b2b9721
Khmelov <Khmelov@users.noreply.github.com>
2016-04-13 10:35:26 +0300

    sync

--- src/projects/akhmelev/servlets/hello.java
+++ src/projects/akhmelev/servlets/hello.java
@@ -1,27 +1,17 @@
-package projects.akhmelev.servlets;
-
-import projects.akhmelev.servlets.dao.Cnn;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.sql.Connection;
-import java.sql.SQLException;
-
-public class hello extends HttpServlet {
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        PrintWriter out = resp.getWriter();
-        out.println("Hello world");
-        out.flush(); //ВАЖНО! Иначе вывод будет неустойчивым...
-        try {
-            Cnn.getConnection().close();
-        } catch (Exception e) {
-            out.println(e+" Bad");
-            e.printStackTrace();
-        }
-    }
-}
+ import java.io.PrintWriter;
+  
+  public class hello extends HttpServlet {
+ -    @Override
+ -    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+ -        PrintWriter out = resp.getWriter();
+ -        out.println("Hello world");
+ -        out.flush(); //ВАЖНО! Иначе вывод будет неустойчивым...
+ +        @Override
+ +        protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+ +            PrintWriter out = resp.getWriter();
+ +            out.println("Hello world");
+ +            out.flush(); //ВАЖНО! Иначе вывод будет неустойчивым...
+  
+ -    }
+ +        }
+  
